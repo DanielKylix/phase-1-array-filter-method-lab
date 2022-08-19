44cf5984f3a56d580f636f61e334f723a6ba1ad4
Daniel <daniel@kylix.online>
2022-08-19 09:04:07 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (drivers, driver) => {
+  const result = drivers.filter((result) => result === driver);
+  return result;
+};
+
+const fuzzyMatch = (drivers, driver) => {
+  let firstChar = driver.charAt(0);
+  const result = drivers.filter((result) => result.startsWith(firstChar));
+  return result;
+};
+
+const matchName = (drivers, driver) => {
+  const result = drivers.filter(function (e) {
+    return e.name === driver;
+  });
+  return result;
+};
